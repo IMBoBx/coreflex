5fb63ed7e00e5ec99be82c0492e57413a1cbdbc5
Garv Kundnani <garvkundnaniyt@gmail.com>
2024-07-01 13:36:29 +0530

    program select section pc

--- app/(home)/programSelect.jsx
+++ app/(home)/programSelect.jsx
@@ -1,24 +1,20 @@
 "use client";
 
 import Link from "next/link";
-import Image from "next/image";
-
 import { useState, useEffect, useMemo } from "react";
 
 import { josefinSans, stickNoBills, urbanist } from "../fonts";
 
 import './home.css';
 
-const _josefinSans = josefinSans.className;
 const _urbanist = urbanist.className;
-const _stickNoBills = stickNoBills.className;
+
+const pilatesPrograms = ["Reformer", "Mat", "Barre"];
+const yogaPrograms = ["Yoga", "Aerial Yoga", "Tabata", "Strength Training"];
+
 
 
 export default function ProgramSelect() {
-
-    // Pilates
-
-    const pilatesPrograms = useMemo(() => ["Reformer", "Mat", "Barre"], []);
 
     const [i, setI] = useState(0);
     const [currentPilates, setPilates] = useState(pilatesPrograms[i]);
@@ -53,23 +49,59 @@ export default function ProgramSelect() {
 
         // Clear the interval when the component unmounts
         return () => clearInterval(interval);
-    }, [pilatesPrograms]);
-    
+    }, []);
 
 
-    const yogaPrograms = ["Yoga", "Aerial Yoga", "Tabata", "Strength Training"];
+    const [j, setJ] = useState(0);
+    const [currentYoga, setYoga] = useState(yogaPrograms[j]);
     
+    useEffect(() => {
+        const yogaElement = document.getElementById("yogaProgram");
+        const interval = setInterval(() => {
+            setJ(prevJ => {
+                const nextJ = prevJ === yogaPrograms.length - 1 ? 0 : prevJ + 1;
+
+                yogaElement.style = `
+                    transform: translateY(25%);
+                    opacity: 0;
+                `;
+                setTimeout(() => {
+                    setYoga(yogaPrograms[nextJ]);
+                    yogaElement.style = `
+                    transform: translateY(-35%);
+                    opacity: 0; 
+                    `;
+                    setTimeout(() => {
+                        yogaElement.style = `
+                        transform: translateY(0);
+                        opacity: 1;
+                        `;
+                    }, 150);
+                }, 150);
+                return nextJ;
+            });
+            
+        }, 1200);
+
+        // Clear the interval when the component unmounts
+        return () => clearInterval(interval);
+    }, []);
+
     return (
-        <section id="programSelectPC" className="flex gap-0 justify-center h-screen overflow-hidden relative"> 
-            <div className="program pilates">
-                <div className={_urbanist}>
-                    <p id="pilatesProgram" className="transition-all swoosh">{currentPilates}</p>
-                </div>
-                <span className={_urbanist}>Pilates</span>
+        <section id="programSelectPC" className={"flex gap-0 justify-center h-screen overflow-hidden relative " + _urbanist}> 
+            <div className="program pilates h-48">
+                <Link className="flex flex-col" href={"#"}>
+                    <div>
+                        <p id="pilatesProgram" className="transition-all h-12 grid place-items-center">{currentPilates}</p>
+                    </div>
+                    <span className="mainProgramText h-36 grid place-items-center">Pilates</span>
+                </Link>
             </div>
 
             <div className="program yoga">
-                {/* <span className="rounded-lg whitespace-nowrap text-ellipsis overflow-scroll">Strength Training</span> */}
+                <Link className="grid place-items-center rounded-lg mainProgramText" href={"#"}>
+                   <p id="yogaProgram" className="transition-all h-48 grid place-items-center">{currentYoga}</p>
+                </Link>
             </div>
         </section>
     );
